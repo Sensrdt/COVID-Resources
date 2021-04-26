694094dbe05cc7e1ba110d7200468f035b1ef1a6
adavijit <adavijit@yahoo.com>
2021-04-26 14:06:43 +0530

    updated

---
+++ src/Utils/WBList.js
@@ -0,0 +1,3 @@
+ const wbcity =["Adra","Alipurduar","Arambagh","Asansol","Baharampur","Balurghat","Bankura","Darjiling","English Bazar","Gangarampur","Habra","Hugli-Chinsurah","Jalpaiguri","Jhargram","Kalimpong","Kharagpur","Kolkata","Mainaguri","Malda","Mathabhanga","Medinipur","Memari","Monoharpur","Murshidabad","Nabadwip","Naihati","Panchla","Pandua","Paschim Punropara","Purulia","Raghunathpur","Raghunathganj","Raiganj","Rampurhat","Ranaghat","Sainthia","Santipur","Siliguri","Sonamukhi","Srirampore","Suri","Taki","Tamluk","Tarakeswar"]
+
+ export default wbcity
--- src/pages/DetailsUpload/DetailsUpload.jsx
+++ src/pages/DetailsUpload/DetailsUpload.jsx
@@ -1,6 +1,58 @@
 import React, { Component } from 'react'
 import './DetailsUpload.css'
+import firebase from '../../config/firebase'
+import wbcitylist from '../../Utils/WBList'
 export class DetailsUpload extends Component {
+    constructor(props) {
+        super(props)
+    
+        this.state = {
+             state: "",
+             city: "",
+             my_contact:"",
+             user_verified: false
+        }
+        this.sendOtp = this.sendOtp.bind(this)
+    }
+
+    sendOtp(e){
+        e.preventDefault();
+        console.log(this.state.my_contact);
+        var recaptcha = new firebase.auth.RecaptchaVerifier('recaptcha',{
+            'size': 'invisible',
+        });
+        var number = '+91'+this.state.my_contact;
+
+        // const appVerifier = window.recaptchaVerifier;
+        firebase.auth().signInWithPhoneNumber(number, recaptcha)
+            .then((confirmationResult) => {
+              // SMS sent. Prompt user to type the code from the message, then sign the
+              // user in with confirmationResult.confirm(code).
+              var code = prompt('Enter the otp', '');
+
+        
+                if(code === null) return;
+
+                
+                confirmationResult.confirm(code).then( (result)=> {
+                    
+                    this.setState({user_verified:true})
+                   
+                }).catch(function (error) {
+                    console.error( error);
+                    
+                });
+
+              // ...
+            }).catch((error) => {
+              // Error; SMS not sent
+              // ...
+            });
+        
+
+       
+    }
+    
     render() {
         return (
             <div>
@@ -20,60 +72,73 @@ export class DetailsUpload extends Component {
                     <legend><span className="number">1</span> Your Basic Info</legend>
                     <label htmlFor="name">Name:</label>
                     <input type="text" id="name" name="user_name" />
-                    <label htmlFor="phone">Phone number:</label>
+                    <label htmlFor="phone">Oxygen phone number:</label>
                     <input type="phone" id="phone" name="phone" />
-                   
-                    <label>Verified:</label>
+
+                    <label htmlFor="phone">Your phone number:</label>
+                    <input type="phone" id="phone" name="phone" 
+                    onChange={(e)=>{
+                        console.log(e.target.value);
+                        this.setState({my_contact: e.target.value})
+                    }}
+                    />
+                    
+                        <label htmlFor="otp">OTP</label>
+                        <input type="text"/>
+                        <button
+                        onClick={this.sendOtp}
+                        className={"verify-btn"} >Send otp</button>
+                    
+                   <br/>
+
+                   <div className={"ox-verified"}>
+                   <label>Verified:</label>
                     <input type="radio" id="under_13" defaultValue="under_13" name="user_age" /><label htmlFor="under_13" className="light">Yes</label><br />
                     <input type="radio" id="over_13" defaultValue="over_13" name="user_age" /><label htmlFor="over_13" className="light">No</label>
+                   </div>
+                   
                   </fieldset>
                   <fieldset>  
                     <legend><span className="number">2</span> Location</legend>
                    
-                    <label htmlFor="job">State:</label>
-                    <select id="job" name="user_job" className={"custom-select"}>
+                    <label htmlFor="state">State:</label>
+                    <select id="state" name="state" className={"custom-select"}
+                    disabled={!this.state.user_verified}
+                    >
                      
-                        <option value="frontend_developer">Front-End Developer</option>
-                        <option value="php_developer">PHP Developer</option>
-                        <option value="python_developer">Python Developer</option>
-                        <option value="rails_developer">Rails Developer</option>
-                        <option value="web_designer">Web Designer</option>
-                        <option value="wordpress_developer">Wordpress Developer</option>
-                    
+                        <option value="West Bengal">West Bengal</option>
                      
                     </select>
 
 
-                    <label htmlFor="job">City:</label>
-                    <select id="job" name="user_job" className={"custom-select"}>
-                     
-                        <option value="frontend_developer">Front-End Developer</option>
-                        <option value="php_developer">PHP Developer</option>
-                        <option value="python_developer">Python Developer</option>
-                        <option value="rails_developer">Rails Developer</option>
-                        <option value="web_designer">Web Designer</option>
-                        <option value="wordpress_developer">Wordpress Developer</option>
+                    <label htmlFor="city">City:</label>
+                    <select id="city" name="city" className={"custom-select"}
+                    disabled={!this.state.user_verified}
+                    
+                    >
+
+                     {wbcitylist.map((city,key)=>{
+                         return <option value={city} key={key}>{city}</option>
+
+                     })}
+                       
                     
                      
                     </select>
 
 
                     <label htmlFor="job">District:</label>
-                    <select id="job" name="user_job" className={"custom-select"}>
-                     
-                        <option value="frontend_developer">Front-End Developer</option>
-                        <option value="php_developer">PHP Developer</option>
-                        <option value="python_developer">Python Developer</option>
-                        <option value="rails_developer">Rails Developer</option>
-                        <option value="web_designer">Web Designer</option>
-                        <option value="wordpress_developer">Wordpress Developer</option>
+                    <input
+                    disabled={!this.state.user_verified}
                     
-                     
-                    </select>
+                    type="text" id="district" name="district" />
 
 
                     <label htmlFor="area">Area:</label>
-                    <input type="text" id="area" name="area" />
+                    <input 
+                    disabled={!this.state.user_verified}
+                    
+                    type="text" id="area" name="area" />
                    
                   </fieldset>
                   <center>
@@ -83,6 +148,8 @@ export class DetailsUpload extends Component {
                 </form>
               </div>
             </div>
+            <div id="recaptcha"></div>
+
           </div>
         
         )

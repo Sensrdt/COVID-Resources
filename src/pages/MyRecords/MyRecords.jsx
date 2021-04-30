cc804c6132cdba9e0b65264277d7175c5be826e1
adavijit <adavijit@yahoo.com>
2021-04-30 22:10:42 +0530

    updated

--- src/pages/MyRecords/MyRecords.jsx
+++ src/pages/MyRecords/MyRecords.jsx
@@ -1,187 +1,175 @@
-import React, { Component } from 'react';
-import List from '../../components/List/List';
-import './MyRecords.css';
-import firebase from '../../config/firebase';
+import React, { Component } from "react";
+import List from "../../components/List/List";
+import "./MyRecords.css";
+import firebase from "../../config/firebase";
 
-import Loader from 'react-loader-spinner';
-import { Modal } from 'react-responsive-modal';
+import Loader from "react-loader-spinner";
+import { Modal } from "react-responsive-modal";
 
-import moment from 'moment'
+import moment from "moment";
 
+import "react-responsive-modal/styles.css";
 
-
-import 'react-responsive-modal/styles.css';
-
-import 'react-loader-spinner/dist/loader/css/react-spinner-loader.css';
-import Navbar from '../../components/Navbar/Navbar';
-import SearchCompoent from '../../components/search/SearchComponent';
+import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
+import Navbar from "../../components/Navbar/Navbar";
+import SearchCompoent from "../../components/search/SearchComponent";
 export class MyRecords extends Component {
-	constructor(props) {
-		super(props);
+  constructor(props) {
+    super(props);
 
-		this.state = {
-			data: [],
-			city: '',
-			loading: false,
-			modal: false,
-			type: '',
-			report_val: 'support',
-			name: '',
-			verified: '',
-			user_verified: '',
-			state: '',
-			district: '',
-			amount: '',
-			quantity: '',
-			cached_city: '',
-			ox_contact: '',
-			updated_on: '',
-			phone: '',
-			not_available: '',
-			support: '',
-			fake: '',
-			cached_type: '',
-			area: '',
-			my_contact: '',
-            remove:false,
-            ukey:null,
-			tempData:[]
+    this.state = {
+      data: [],
+      city: "",
+      loading: false,
+      modal: false,
+      type: "",
+      report_val: "support",
+      name: "",
+      verified: "",
+      user_verified: "",
+      state: "",
+      district: "",
+      amount: "",
+      quantity: "",
+      cached_city: "",
+      ox_contact: "",
+      updated_on: "",
+      phone: "",
+      not_available: "",
+      support: "",
+      fake: "",
+      cached_type: "",
+      area: "",
+      my_contact: "",
+      remove: false,
+      ukey: null,
+      tempData: [],
+    };
+    this.fetchUniversalData = this.fetchUniversalData.bind(this);
 
-		};
-		this.fetchUniversalData = this.fetchUniversalData.bind(this);
+    this.myRootRef = React.createRef();
+  }
 
-		this.myRootRef = React.createRef();
-	}
-
-    
-	componentDidMount() {
-
-        if(sessionStorage.getItem('co_aiduser')){
-            this.setState({
-                ...this.state,
-                my_contact: sessionStorage.getItem('co_aiduser')
-
-            });
-		this.fetchUniversalData();
-
-        }
-
-		
-	}
-
-    ParseDate=(dateString)=> {
-        if(moment(dateString,'DD-MM-YYYY HH:mm:ss').isValid()){
-
-         
-            let now2 =moment(dateString,'DD/MM/YYYY HH:mm:ss').format("MM/DD/YYYY HH:mm:ss");
-
-     
-
-            return (Date.parse(now2));
-          
-        }else if(moment(dateString,'MM/DD/YYYY HH:mm:ss').isValid()){
-            return (Date.parse(dateString));
-
-        }
-        else if(moment(dateString,'DD/MM/YYYY HH:mm:ss').isValid()){
-            return(Date.parse(dateString));
-
-        }
-        else{
-            return(Date.parse(dateString));
-            
-        }
+  componentDidMount() {
+    if (sessionStorage.getItem("co_aiduser")) {
+      this.setState({
+        ...this.state,
+        my_contact: sessionStorage.getItem("co_aiduser"),
+      });
+      this.fetchUniversalData();
     }
-    getSortedArray(arr){
+  }
 
-        const tempArr = [...arr]
+  ParseDate = (dateString) => {
+    if (moment(dateString, "DD-MM-YYYY HH:mm:ss").isValid()) {
+      let now2 = moment(dateString, "DD/MM/YYYY HH:mm:ss").format(
+        "MM/DD/YYYY HH:mm:ss"
+      );
 
-        tempArr.map(data=>{
-           return data.updated_on= this.ParseDate(data.updated_on)
-            
-        })
-
-
-        // tempArr.sort(function (left, right) {
-        //     return moment.utc(left.timeStamp).diff(moment.utc(right.timeStamp))
-        // });
-
-        tempArr.sort((a,b) => new Date(a.updated_on).getTime() - new Date(b.updated_on).getTime() )
-
-        console.log(tempArr);
-        return tempArr.reverse()
+      return Date.parse(now2);
+    } else if (moment(dateString, "MM/DD/YYYY HH:mm:ss").isValid()) {
+      return Date.parse(dateString);
+    } else if (moment(dateString, "DD/MM/YYYY HH:mm:ss").isValid()) {
+      return Date.parse(dateString);
+    } else {
+      return Date.parse(dateString);
     }
+  };
+  getSortedArray(arr) {
+    const tempArr = [...arr];
 
+    tempArr.map((data) => {
+      return (data.updated_on = this.ParseDate(data.updated_on));
+    });
 
-    fetchUniversalData() {
-		this.setState({
-			...this.state,
-			modal: true,
-			loading: true,
-		});
-        const databaseRef = firebase.database().ref('/data2').child(sessionStorage.getItem('co_aiduser'));
+    // tempArr.sort(function (left, right) {
+    //     return moment.utc(left.timeStamp).diff(moment.utc(right.timeStamp))
+    // });
 
-		databaseRef.on(
-			'value',
-			(snapshot) => {
-				let json = snapshot.val();
-				console.log(json);
-				const arr = [];
-				try {
-                    Object.keys(json).forEach(function (key) {
-						arr.push(json[key]);
-					});
-                    const data = this.getSortedArray(arr)||[]
-					this.setState({
-						data: data,
-						modal: false,
-						loading: false,
-			            tempData:data
+    tempArr.sort(
+      (a, b) =>
+        new Date(a.updated_on).getTime() - new Date(b.updated_on).getTime()
+    );
 
-					});
-				} catch (error) {
-					this.setState({
-						data: [],
-						modal: false,
-						loading: false,
-					});
-				}
+    console.log(tempArr);
+    return tempArr.reverse();
+  }
 
-			},
-			function (errorObject) {
-				console.log('The read failed: ' + errorObject.code);
-			}
-		);
-	}
+  fetchUniversalData() {
+    this.setState({
+      ...this.state,
+      modal: true,
+      loading: true,
+    });
+    const databaseRef = firebase
+      .database()
+      .ref("/data2")
+      .child(sessionStorage.getItem("co_aiduser"));
 
-	componentWillUnmount() {
-		this.setState({
-			data: [],
-			city: '',
-			loading: true,
-			modal: false,
-			type: '',
-		});
-	}
+    databaseRef.on(
+      "value",
+      (snapshot) => {
+        let json = snapshot.val();
+        console.log(json);
+        const arr = [];
+        try {
+          Object.keys(json).forEach(function (key) {
+            arr.push(json[key]);
+          });
+          const data = this.getSortedArray(arr) || [];
+          this.setState({
+            data: data,
+            modal: false,
+            loading: false,
+            tempData: data,
+          });
+        } catch (error) {
+          this.setState({
+            data: [],
+            modal: false,
+            loading: false,
+          });
+        }
+      },
+      function (errorObject) {
+        console.log("The read failed: " + errorObject.code);
+      }
+    );
+  }
 
-	onCloseModal = () => {
-		this.setState({ modal: false });
-	};
+  componentWillUnmount() {
+    this.setState({
+      data: [],
+      city: "",
+      loading: true,
+      modal: false,
+      type: "",
+    });
+  }
 
-	remove=()=>{
-        let databaseRef = firebase.database().ref('/data2').child(sessionStorage.getItem('co_aiduser')).child(this.state.ukey).remove()
-    }
+  onCloseModal = () => {
+    this.setState({ modal: false });
+  };
 
-    setData = (data)=>{
-		this.setState({
-			data
-		})
-	}
+  remove = () => {
+    let databaseRef = firebase
+      .database()
+      .ref("/data2")
+      .child(sessionStorage.getItem("co_aiduser"))
+      .child(this.state.ukey)
+      .remove();
+  };
 
-	render() {
-		return (
-			<div className={'listing-main'}>
-				{/* <Fab
+  setData = (data) => {
+    this.setState({
+      data,
+    });
+  };
+
+  render() {
+    return (
+      <div className={"listing-main"}>
+        {/* <Fab
 					alwaysShowTitle={true}
 					icon={<Upload />}
 					onClick={() => {
@@ -192,78 +180,82 @@ export class MyRecords extends Component {
 							behavior: 'smooth',
 						});
 					}}></Fab> */}
-				<Modal
-					open={this.state.modal}
-					onClose={this.onCloseModal}
-					center={true}
-					closeOnOverlayClick={false}
-					showCloseIcon={false}>
-					{this.state.loading ? (
-						<Loader type='Puff' color='#4a74c9' height={100} width={100} />
-					) : (
-						''
-					)}
+        <Modal
+          open={this.state.modal}
+          onClose={this.onCloseModal}
+          center={true}
+          closeOnOverlayClick={false}
+          showCloseIcon={false}
+        >
+          {this.state.loading ? (
+            <Loader type="Puff" color="#4a74c9" height={100} width={100} />
+          ) : (
+            ""
+          )}
 
-
-                    {this.state.remove?
-                <div>
-                   <p>Are you sure?</p> 
-                    <div className={"remove-div"}>
-                        <a href="#"
-                        onClick={()=>this.setState({ modal: false ,remove:false,ukey:null})}
-                        >No</a> <button
-                        onClick={this.remove}
-                        >Yes</button>
-                    </div>
-                </div>    :""
-                
-                }
-					
-				</Modal>
-                <Navbar
-               redirect={()=>this.props.history.push(`/`)}
-               />
-				<div className={'refresh'}>
-					<p onClick={() => this.props.history.push(`/`)}>Back to home</p>
-					<p onClick={() => window.location.reload()}>Refresh</p>
-				</div>
-                <SearchCompoent data = {this.state.tempData} setData={this.setData}/>
-				<div ref={this.myRootRef}>
-                    {this.state.data.length===0 && !this.state.loading?
-                <p className={"no-data"}>Sorry 😔 <br/>Please try after sometime</p>:
-                        ""}
-					{this.state.data.map((value,index) => {
-						return (
-							<List
-                                delete={true}
-								name={value.name}
-								verified={value.verified}
-								user_verified={value.user_verified}
-								city={value.city}
-								state={value.state}
-								district={value.district}
-								ox_contact={value.ox_contact}
-								phone={value.my_contact}
-								updated_on={value.updated_on}
-								amount={value.amount}
-								area={value.area}
-								quantity={value.quantity}
-								type={value.type}
-                                ukey={value.key}
-								key= {index}
-                                onDelete={(ukey)=>{
-                                    
-                                    this.setState({ modal: true ,remove:true,ukey:ukey});
-                                
-                                   
-                                }}
-							/>
-						);
-					})}
-				</div>
-			</div>
-		);
-	}
+          {this.state.remove ? (
+            <div>
+              <p>Are you sure?</p>
+              <div className={"remove-div"}>
+                <a
+                  href="#"
+                  onClick={() =>
+                    this.setState({ modal: false, remove: false, ukey: null })
+                  }
+                >
+                  No
+                </a>{" "}
+                <button onClick={this.remove}>Yes</button>
+              </div>
+            </div>
+          ) : (
+            ""
+          )}
+        </Modal>
+        <Navbar redirect={() => this.props.history.push(`/`)} />
+        <div className={"refresh"}>
+          <p onClick={() => this.props.history.push(`/`)}>Back to home</p>
+          <p onClick={() => window.location.reload()}>Refresh</p>
+        </div>
+        <SearchCompoent data={this.state.tempData} setData={this.setData} />
+        <div ref={this.myRootRef}>
+          {this.state.data.length === 0 && !this.state.loading ? (
+            <p className={"no-data"}>
+              Sorry 😔 <br />
+              Please try after sometime
+            </p>
+          ) : (
+            ""
+          )}
+          {this.state.data.map((value, index) => {
+            return (
+              <List
+                delete={true}
+                name={value.name}
+                verified={value.verified}
+                user_verified={value.user_verified}
+                city={value.city}
+                state={value.state}
+                district={value.district}
+                ox_contact={value.ox_contact}
+                phone={value.my_contact}
+                updated_on={value.updated_on}
+                amount={value.amount}
+                area={value.area}
+                quantity={value.quantity}
+                type={value.type}
+                ukey={value.key}
+                key={index}
+                onDelete={(ukey) => {
+                  this.setState({ modal: true, remove: true, ukey: ukey });
+                }}
+              />
+            );
+          })}
+        </div>
+      </div>
+    );
+  }
 }
 
 export default MyRecords;
